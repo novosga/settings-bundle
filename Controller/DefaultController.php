182f8d76663393ef191c9e9d5b71a96b6c84ebfc
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2018-01-04 21:20:09 -0200

    phpcs

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -255,7 +255,7 @@ class DefaultController extends Controller
             ->getManager()
             ->getRepository(Contador::class)
             ->findOneBy([
-                'unidade' => $unidade, 
+                'unidade' => $unidade,
                 'servico' => $servico,
             ]);
 
