7feaceda3ae541d95176557783ed68f491f90e18
Rogerio Lino <rogeriolino@gmail.com>
2026-03-24 10:31:46 -0300

    chore: symfony 7.4 deprecations

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -38,7 +38,7 @@ use Novosga\SettingsBundle\Form\ImpressaoType;
 use Novosga\SettingsBundle\Form\ServicoUnidadeType;
 use Novosga\SettingsBundle\NovosgaSettingsBundle;
 use Psr\Clock\ClockInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
